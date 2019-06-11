1166812c9b448f649a8130eb366d63cb0af567c5
Johnathon Wood <jaywood128@gmail.com>
2019-06-11 19:22:40 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user, :only [:create]
+  skip_before_action :authenticate_user, only: [:create]
 
   def create
     response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
       req.body = { 'client_id': '99749e9404eb59033620', 'client_secret': '2c7eb860cbd3e76f88fa8d3fd09a00ba0d2741ba', 'code': code }
       req.headers['Accept'] = 'application/json'
+    end
   end
 end
