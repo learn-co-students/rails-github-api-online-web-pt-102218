52c2f2a234a365c9c41572d7b5a42eec3c067718
Johnathon Wood <jaywood128@gmail.com>
2019-06-12 12:23:23 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,11 @@
 class RepositoriesController < ApplicationController
   
   def index
+    @username = session[:username]
+    users_repos = Faraday.get "https://api.github.com/user/repos" do |req| 
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    @repos = JSON.parse(users_repos.body)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,18 @@ class SessionsController < ApplicationController
       req.body = { 'client_id': '99749e9404eb59033620', 'client_secret': '2c7eb860cbd3e76f88fa8d3fd09a00ba0d2741ba', 'code': params[:code] }
       req.headers['Accept'] = 'application/json'
     end
+    body = JSON.parse(response.body)
+    session[:token] = body['access_token']
+    
+    user_response = Faraday.get "https://api.github.com/user" do |req| 
+      req.body = { 'client_id': '99749e9404eb59033620', 'client_secret': '2c7eb860cbd3e76f88fa8d3fd09a00ba0d2741ba', 'code': params[:code] }
+      req.headers['Accept'] = 'application/json'
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end 
+    
+    @user_response = JSON.parse(user_response.body)
+   session[:username] = @user_response["login"]
+  
+    redirect_to root_path 
   end
 end
