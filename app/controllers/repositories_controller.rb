2737af594a813941d50df6b393bb1e268a67c560
Paul Ashour <paul.ashour@gmail.com>
2019-07-01 11:10:36 -0600

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,19 @@
 class RepositoriesController < ApplicationController
   
   def index
+    # retrieve and display the current user's 'login'
+    response = Faraday.get "https://api.github.com/user" do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    body = JSON.parse(response.body)
+    @login = body["login"]
+
+    # retrieve the current user's repositories
+    rep_response = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    body = JSON.parse(rep_response.body)
+    @repos = body.map{ |r| r["name"] }
   end
 
 end
